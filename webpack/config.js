66a2af04132b182b3e6caaf694f3204fdf81c0ed
leeching <leeching.fx@gmail.com>
2017-04-02 21:32:25 +0800

    update webpack.config

--- webpack/config.js
+++ webpack/config.js
@@ -6,6 +6,79 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
 
+let devtool = 'source-map';
+let outputPath = '/';
+let filename = '[name].js';
+
+const rules = [
+  {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /node_modules/,
+    options: {
+      presets: ['es2015', 'stage-2'],
+      plugins: ['transform-runtime'],
+    },
+  },
+  {
+    test: /\.sass$/,
+    loader: ExtractTextPlugin.extract({
+      use: [
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            minimize: isProd ? true : false,
+          },
+        },
+        'postcss-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        }
+      ]
+    }),
+  },
+];
+
+const plugins = [
+  new webpack.ProvidePlugin({
+    'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+  }),
+  new ManifestPlugin(),
+  new ExtractTextPlugin({
+    filename: isProd ? '[contenthash].css' : '[name].css',
+  }),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  }),
+  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: Infinity,
+    filename: isProd ? '[chunkhash].js' : '[name].js',
+  }),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      postcss: [
+        autoprefixer({
+          browsers: ['last 2 versions'],
+        }),
+      ],
+    },
+  }),
+];
+
+if (isProd) {
+  outputPath = path.join(__dirname, 'dist');
+  filename = 'name-[chunkhash:8].js';
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
 module.exports = {
   entry: {
     vendor: [
@@ -28,68 +101,6 @@ module.exports = {
       '.js', '.json', '.sass'
     ],
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['es2015', 'stage-2'],
-          plugins: ['transform-runtime'],
-        },
-      },
-      {
-        test: /\.sass$/,
-        loader: ExtractTextPlugin.extract({
-          use: ['css-loader', 'postcss-loader', 'sass-loader'],
-        }),
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          use: ['css-loader', 'postcss-loader'],
-        }),
-      },
-    ],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
-    }),
-    new ManifestPlugin(),
-    new ExtractTextPlugin({
-      filename: isProd ? '[contenthash].css' : '[name].css',
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity,
-      filename: isProd ? '[chunkhash].js' : '[name].js',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        postcss: [
-          autoprefixer({
-            browsers: ['last 2 versions'],
-          }),
-        ],
-      },
-    })
-  ],
+  module: { rules },
+  plugins,
 };
-
-if (!isProd) {
-  module.exports.devtool = '#source-map';
-}
-
-if (isProd) {
-  module.exports.output.path = path.join(__dirname, 'dist');
-  module.exports.output.filename = '[chunkhash].js';
-  module.exports.plugins.push(
-    new webpack.optimize.UglifyJsPlugin()
-  );
-}
